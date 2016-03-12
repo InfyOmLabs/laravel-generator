6591a3fc459e10b383b69b40b18523dcd18cfb8d
Mitul Golakya <mtl.golakiya@gmail.com>
2016-03-12 11:43:59 +0530

    fixed #61

--- src/Utils/GeneratorFieldsInputUtil.php
+++ src/Utils/GeneratorFieldsInputUtil.php
@@ -34,7 +34,7 @@ class GeneratorFieldsInputUtil
                 $searchable = false;
             }
 
-            if (isset($field['searchable'])) {
+            if (isset($field['fillable'])) {
                 $fillable = $field['fillable'];
             } else {
                 $fillable = false;
