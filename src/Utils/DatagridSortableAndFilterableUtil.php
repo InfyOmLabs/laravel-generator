36c712df47991fc0679fe3834986383238570b74
Shem Pasamba <shemgp@yahoo.com>
2016-09-18 20:35:34 +0800

    Add setDefaultOrder method

--- src/Utils/DatagridSortableAndFilterableUtil.php
+++ src/Utils/DatagridSortableAndFilterableUtil.php
@@ -7,34 +7,48 @@ use Illuminate\Http\Request;
 trait DatagridSortableAndFilterableUtil
 {
     /**
-      * Convert datagrid filters to repository filters.
-      *
-      * @param Request &$request
-      */
-     private function convertDatagridFilterToRepositoryFilter(Request &$request)
-     {
-         if (!isset($request['f'])) {
-             return;
-         }
+     * Convert datagrid filters to repository filters.
+     *
+     * @param Request &$request
+     */
+    private function convertDatagridFilterToRepositoryFilter(Request &$request)
+    {
+        if (!isset($request['f'])) {
+            return;
+        }
 
-         $f = $request['f'];
+        $f = $request['f'];
 
-         if (isset($f['order_by']) && !empty($f['order_by'])
-             && isset($f['order_dir']) && !empty($f['order_dir'])) {
-             $request['orderBy'] = $f['order_by'];
-             $request['sortedBy'] = $f['order_dir'] !== 'DESC' ? 'asc' : 'desc';
-         }
+        if (isset($f['order_by']) && !empty($f['order_by'])
+            && isset($f['order_dir']) && !empty($f['order_dir'])) {
+                $request['orderBy'] = $f['order_by'];
+                $request['sortedBy'] = $f['order_dir'] !== 'DESC' ? 'asc' : 'desc';
+        }
 
-         unset($f['order_by']);
-         unset($f['order_dir']);
-         $searches = [];
-         foreach ($f as $field => $value) {
-             if ($value == '') {
+        unset($f['order_by']);
+        unset($f['order_dir']);
+        $searches = [];
+        foreach ($f as $field => $value) {
+            if ($value == '') {
                  continue;
-             }
-             $searches[] = $field.':'.$value;
-         }
-         $request['search'] = implode(';', $searches);
-         $request['searchUseAnd'] = 1;
-     }
+            }
+            $searches[] = $field.':'.$value;
+        }
+        $request['search'] = implode(';', $searches);
+        $request['searchUseAnd'] = 1;
+    }
+
+    /**
+     * Set default order by and direction
+     *
+     * @param Request $request
+     */
+    private function setDefaultOrder(&$request, $by, $dir = 'ASC')
+    {
+        if (!isset($request['f']['order_by']) ||
+            isset($request['f']['order_by']) && $request['f']['order_by'] == '') {
+                $request['orderBy'] = $by;
+                $request['sortedBy'] = $dir;
+        }
+    }
 }
