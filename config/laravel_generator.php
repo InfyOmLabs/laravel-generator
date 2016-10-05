3f6dc943b7bca5357a516567314608f2a270cd12
Mitul Golakya <mtl.golakiya@gmail.com>
2016-10-05 09:52:34 +0530

    default adminlte templates and menu published

--- config/laravel_generator.php
+++ config/laravel_generator.php
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'templates'         => 'core-templates',
+    'templates'         => 'adminlte-templates',
 
     /*
     |--------------------------------------------------------------------------
@@ -148,7 +148,7 @@ return [
 
         'menu'          => [
 
-            'enabled'       => false,
+            'enabled'       => true,
 
             'menu_file'     => 'layouts/menu.blade.php',
         ],
