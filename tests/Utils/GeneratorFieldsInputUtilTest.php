e41340c1a3965d007944812f67629465c6522bc2
Mitul Golakiya <mtl.golakiya@gmail.com>
2018-06-06 00:19:35 +0530

    some more tests added

--- tests/Utils/GeneratorFieldsInputUtilTest.php
+++ tests/Utils/GeneratorFieldsInputUtilTest.php
@@ -8,18 +8,36 @@ class GeneratorFieldsInputUtilTest extends \PHPUnit_Framework_TestCase
 {
     public function testValidateFieldInput()
     {
-        // invalid
-        // only name, without database type
+        // invalid, only name, without database type
         $input = 'title';
 
         $res = GeneratorFieldsInputUtil::validateFieldInput($input);
         $this->assertFalse($res);
 
-        // valid
-        // with database type
+        // valid, with database type
         $input = 'title string';
 
         $res = GeneratorFieldsInputUtil::validateFieldInput($input);
         $this->assertTrue($res);
+    }
+
+    public function testProcessFieldInput()
+    {
+        // name, db_type and html_type
+        $input = 'title string text';
+        $validations = 'required';
+
+        $res = GeneratorFieldsInputUtil::processFieldInput($input, $validations);
+        $this->assertEquals($validations, $res->validations);
+        $this->assertEquals('title', $res->name);
+
+        $this->assertEquals('string', $res->dbInput);
+        $this->assertEquals('$table->string(\'title\');', $res->migrationText);
+
+        $this->assertEquals('text', $res->htmlType);
+        $this->assertEquals('text', $res->htmlType);
+        $this->assertEmpty($res->htmlValues);
+
+        //
     }
 }
