17e9c922228f8a6d0280c3e72e9dd19434adf271
ajayinfyom <ajay.makwana@infyom.com>
2019-03-14 18:50:21 +0530

    fix(rollback): #479 fix rollback of custom plural model

--- src/Commands/RollbackGeneratorCommand.php
+++ src/Commands/RollbackGeneratorCommand.php
@@ -80,7 +80,7 @@ class RollbackGeneratorCommand extends Command
         $this->commandData = new CommandData($this, $this->argument('type'));
         $this->commandData->config->mName = $this->commandData->modelName = $this->argument('model');
 
-        $this->commandData->config->init($this->commandData, ['tableName', 'prefix']);
+        $this->commandData->config->init($this->commandData, ['tableName', 'prefix', 'plural']);
 
         $migrationGenerator = new MigrationGenerator($this->commandData);
         $migrationGenerator->rollback();
