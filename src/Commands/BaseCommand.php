f5262a89bf7fb4eb63c207483f6e3fa49592bd1e
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-03-15 14:01:10 +0530

    feat(model): #479 support custom plural model name (#610)

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -228,6 +228,7 @@ class BaseCommand extends Command
         return [
             ['fieldsFile', null, InputOption::VALUE_REQUIRED, 'Fields input as json file'],
             ['jsonFromGUI', null, InputOption::VALUE_REQUIRED, 'Direct Json string while using GUI interface'],
+            ['plural', null, InputOption::VALUE_REQUIRED, 'Plural Model name'],
             ['tableName', null, InputOption::VALUE_REQUIRED, 'Table Name'],
             ['fromTable', null, InputOption::VALUE_NONE, 'Generate from existing table'],
             ['ignoreFields', null, InputOption::VALUE_REQUIRED, 'Ignore fields while generating from table'],
