0f3b2a912386bc6a047c6963298014755152ac6e
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-03-22 11:40:49 +0530

    fix(relations): #475 Fix relationship method names as per foreign key (#616)

--- src/Utils/TableFieldsGenerator.php
+++ src/Utils/TableFieldsGenerator.php
@@ -517,7 +517,9 @@ class TableFieldsGenerator
 
             if ($foreignField == $tables[$foreignTable]->primaryKey) {
                 $modelName = model_name_from_table_name($foreignTable);
-                $manyToOneRelations[] = GeneratorFieldRelation::parseRelation('mt1,'.$modelName);
+                $manyToOneRelations[] = GeneratorFieldRelation::parseRelation(
+                    'mt1,'.$modelName.','.$foreignKey->localField
+                );
             }
         }
 
