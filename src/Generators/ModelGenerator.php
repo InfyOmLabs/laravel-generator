fcb091f6ede3045c602098abaea11b2ebc25b907
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-03-22 11:40:49 +0530

    fix(relations): #475 Fix relationship method names as per foreign key (#616)

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -158,12 +158,17 @@ class ModelGenerator extends BaseGenerator
                 return 'string';
             case '1t1':
             case 'mt1':
-                return '\\'.$this->commandData->config->nsModel.'\\'.$relation->inputs[0].' '.camel_case($relation->inputs[0]);
+                if (isset($relation->inputs[1])) {
+                    $relationName = str_replace('_id', '', strtolower($relation->inputs[1]));
+                } else {
+                    $relationName = $relation->inputs[0];
+                }
+
+                return '\\'.$this->commandData->config->nsModel.'\\'.$relation->inputs[0].' '.camel_case($relationName);
             case '1tm':
-                return '\Illuminate\Database\Eloquent\Collection'.' '.$relation->inputs[0];
             case 'mtm':
             case 'hmt':
-                return '\Illuminate\Database\Eloquent\Collection'.' '.camel_case($relation->inputs[1]);
+                return '\Illuminate\Database\Eloquent\Collection'.' '.camel_case(str_plural($relation->inputs[0]));
             default:
                 return $db_type;
         }
@@ -306,7 +311,6 @@ class ModelGenerator extends BaseGenerator
 
         foreach ($this->commandData->relations as $relation) {
             $relationText = $relation->getRelationFunctionText();
-
             if (!empty($relationText)) {
                 $relations[] = $relationText;
             }
