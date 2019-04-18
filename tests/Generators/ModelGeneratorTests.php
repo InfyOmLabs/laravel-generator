9e42a6c765b4b5f8cee93c418eef960ef0fe1b74
vishalinfyom <vishal.ribdiya@infyom.com>
2019-04-18 10:58:21 +0530

    refactor: code refactored

--- tests/Generators/ModelGeneratorTests.php
+++ tests/Generators/ModelGeneratorTests.php
@@ -5,9 +5,12 @@ namespace Tests\Generators;
 use InfyOm\Generator\Generators\ModelGenerator;
 use PHPUnit_Framework_TestCase;
 use stdClass;
+use Tests\Traits\CommonTrait;
 
 class ModelGeneratorTests extends PHPUnit_Framework_TestCase
 {
+    use CommonTrait;
+
     public function testGetPhpDocType()
     {
         // prepare properties to set into model
@@ -16,22 +19,23 @@ class ModelGeneratorTests extends PHPUnit_Framework_TestCase
         $object->config->nsModel = 'App\Models';
 
         // mock model and set properties
+        /** @var ModelGenerator $modelGenerator */
         $modelGenerator = $this->mockClassExceptMethods(ModelGenerator::class, ['getPHPDocType']);
         $modelGenerator->commandData = $object;
 
         $inputs = ['datetime' => 'string|\Carbon\Carbon', 'string' => 'string'];
-        foreach ($inputs as $dbType => $input) {
+        foreach ($inputs as $dbType => $expected) {
             $response = $modelGenerator->getPHPDocType($dbType);
-            $this->assertEquals($input, $response);
+            $this->assertEquals($expected, $response);
         }
 
         $relationObj = new stdClass();
-        $relationObj->inputs[0] = 'ModelName';
-        $relationObj->inputs[1] = 'relation_id';
+        $relationObj->inputs[0] = 'Business'; // model name
+        $relationObj->inputs[1] = 'business_id'; // foreign key
 
         $inputs = [
-            '\App\Models\ModelName relation'                      => ['1t1', 'mt1'],
-            '\Illuminate\Database\Eloquent\Collection modelNames' => ['1tm', 'mtm', 'hmt'],
+            '\App\Models\Business business'                       => ['1t1', 'mt1'],
+            '\Illuminate\Database\Eloquent\Collection businesses' => ['1tm', 'mtm', 'hmt'],
         ];
         foreach ($inputs as $expected => $dbTypes) {
             foreach ($dbTypes as $dbType) {
@@ -43,6 +47,7 @@ class ModelGeneratorTests extends PHPUnit_Framework_TestCase
 
     public function testReturnGivenTypeItSelfWhenNoMatchingTypesFound()
     {
+        /** @var ModelGenerator $modelGenerator */
         $modelGenerator = $this->mockClassExceptMethods(ModelGenerator::class, ['getPHPDocType']);
 
         $response = $modelGenerator->getPHPDocType('integer');
@@ -52,53 +57,31 @@ class ModelGeneratorTests extends PHPUnit_Framework_TestCase
     public function testGenerateRequireFields()
     {
         $fields = $this->prepareFields([
-            ['name' => 'non_required_field', 'validations' => ''],
-            ['name' => 'required_field', 'validations' => 'required'],
+            ['name' => 'business_id', 'validations' => ''], // optional field
+            ['name' => 'location_id', 'validations' => 'required'], // required field should return
         ]);
 
+        /** @var ModelGenerator $modelGenerator */
         $modelGenerator = $this->mockClassExceptMethods(ModelGenerator::class, ['generateRequiredFields']);
         $modelGenerator->commandData = $fields;
 
         $response = $modelGenerator->generateRequiredFields();
-        $this->assertContains('required_field', $response);
+        $this->assertContains('location_id', $response);
     }
 
     public function testReturnEmptyWhenAllFieldAreNonRequired()
     {
+        // both fields are optional
         $fields = $this->prepareFields([
-            ['name' => 'non_required_field_1', 'validations' => ''],
-            ['name' => 'non_required_field_2', 'validations' => ''],
+            ['name' => 'business_id', 'validations' => ''],
+            ['name' => 'location_id', 'validations' => ''],
         ]);
 
+        /** @var ModelGenerator $modelGenerator */
         $modelGenerator = $this->mockClassExceptMethods(ModelGenerator::class, ['generateRequiredFields']);
         $modelGenerator->commandData = $fields;
 
         $response = $modelGenerator->generateRequiredFields();
         $this->assertEmpty($response);
-    }
-
-    public function mockClassExceptMethods($className, $methods)
-    {
-        return $this->getMockBuilder($className)
-            ->setMethodsExcept($methods)
-            ->disableOriginalConstructor()
-            ->getMock();
-    }
-
-    public function prepareFields($fields)
-    {
-        $objects = [];
-        foreach ($fields as $field) {
-            $object = new stdClass();
-            foreach ($field as $key => $value) {
-                $object->$key = $value;
-            }
-            $objects[] = $object;
-        }
-
-        $fields = new stdClass();
-        $fields->fields = $objects;
-
-        return $fields;
     }
 }
---
+++ tests/Traits/CommonTrait.php
@@ -0,0 +1,27 @@
+<?php namespace Tests\Traits;
+
+use stdClass;
+
+trait CommonTrait
+{
+    public function mockClassExceptMethods($className, $methods)
+    {
+        return $this->getMockBuilder($className)
+            ->setMethodsExcept($methods)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function prepareFields($fields)
+    {
+        $objects = [];
+        foreach ($fields as $field) { // $field should contain array e.g [property1 => value, property2 => value, ....]
+            $objects[] = (object) $field; // convert array to object
+        }
+
+        $fields = new stdClass();
+        $fields->fields = $objects;
+
+        return $fields;
+    }
+}
