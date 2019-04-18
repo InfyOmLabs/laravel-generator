88e1cffe13546c4f819b2cd801c19b510088ef27
vishalinfyom <vishal.ribdiya@infyom.com>
2019-04-18 11:10:28 +0530

    refactor: code refactored

--- tests/Generators/TestTraitGeneratorTest.php
+++ tests/Generators/TestTraitGeneratorTest.php
@@ -13,14 +13,14 @@ class TestTraitGeneratorTest extends PHPUnit_Framework_TestCase
     public function testGenerateFields()
     {
         $fields = $this->prepareFields([
-            ['name' => 'filed_name_1', 'fieldType' => 'integer', 'isPrimary' => false],
-            ['name' => 'filed_name_2', 'fieldType' => 'float', 'isPrimary' => false],
-            ['name' => 'filed_name_3', 'fieldType' => 'string', 'isPrimary' => false],
-            ['name' => 'filed_name_4', 'fieldType' => 'text', 'isPrimary' => false],
-            ['name' => 'filed_name_5', 'fieldType' => 'datetime', 'isPrimary' => false],
-            ['name' => 'filed_name_6', 'fieldType' => 'timestamp', 'isPrimary' => false],
+            ['name' => 'field_name_1', 'fieldType' => 'integer', 'isPrimary' => false],
+            ['name' => 'field_name_2', 'fieldType' => 'float', 'isPrimary' => false],
+            ['name' => 'field_name_3', 'fieldType' => 'string', 'isPrimary' => false],
+            ['name' => 'field_name_4', 'fieldType' => 'text', 'isPrimary' => false],
+            ['name' => 'field_name_5', 'fieldType' => 'datetime', 'isPrimary' => false],
+            ['name' => 'field_name_6', 'fieldType' => 'timestamp', 'isPrimary' => false],
             [
-                'name'       => 'filed_name_7',
+                'name'       => 'field_name_7',
                 'fieldType'  => 'enum',
                 'isPrimary'  => false,
                 'htmlValues' => ['value1', 'value2'],
@@ -31,13 +31,13 @@ class TestTraitGeneratorTest extends PHPUnit_Framework_TestCase
         $traitGenerator->commandData = $fields;
 
         $expectedOutput = [
-            "'filed_name_1' => ".'$fake->randomDigitNotNull',
-            "'filed_name_2' => ".'$fake->randomDigitNotNull',
-            "'filed_name_3' => ".'$fake->word',
-            "'filed_name_4' => ".'$fake->text',
-            "'filed_name_5' => ".'$fake->date('."'Y-m-d H:i:s'".')',
-            "'filed_name_6' => ".'$fake->date('."'Y-m-d H:i:s'".')',
-            "'filed_name_7' => ".'$fake->randomElement(['."'value1', "."'value2'".'])',
+            "'field_name_1' => ".'$fake->randomDigitNotNull',
+            "'field_name_2' => ".'$fake->randomDigitNotNull',
+            "'field_name_3' => ".'$fake->word',
+            "'field_name_4' => ".'$fake->text',
+            "'field_name_5' => ".'$fake->date('."'Y-m-d H:i:s'".')',
+            "'field_name_6' => ".'$fake->date('."'Y-m-d H:i:s'".')',
+            "'field_name_7' => ".'$fake->randomElement(['."'value1', "."'value2'".'])',
         ];
 
         $response = $traitGenerator->generateFields();
@@ -51,7 +51,7 @@ class TestTraitGeneratorTest extends PHPUnit_Framework_TestCase
     public function testGenerateFieldAsTypeWordWhenNoMatchingTypesFound()
     {
         $fields = $this->prepareFields([
-            ['name' => 'filed_name', 'fieldType' => 'db_type_not_found', 'isPrimary' => false],
+            ['name' => 'field_name', 'fieldType' => 'db_type_not_found', 'isPrimary' => false],
         ]);
 
         $traitGenerator = $this->mockClassExceptMethods(TestTraitGenerator::class, ['generateFields']);
@@ -60,13 +60,13 @@ class TestTraitGeneratorTest extends PHPUnit_Framework_TestCase
         $response = $traitGenerator->generateFields();
 
         $this->assertCount(1, $response);
-        $this->assertEquals("'filed_name' => ".'$fake->word', $response[0]);
+        $this->assertEquals("'field_name' => ".'$fake->word', $response[0]);
     }
 
     public function testNotGenerateFieldsForPrimaryField()
     {
         $fields = $this->prepareFields([
-            ['name' => 'filed_name', 'fieldType' => 'integer', 'isPrimary' => true],
+            ['name' => 'field_name', 'fieldType' => 'integer', 'isPrimary' => true],
         ]);
 
         $traitGenerator = $this->mockClassExceptMethods(TestTraitGenerator::class, ['generateFields']);
