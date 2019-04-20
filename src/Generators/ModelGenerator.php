a9907565358fccf1a79b1401d1b3e347e87b16fb
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-04-20 12:26:37 +0530

    fix(relationship): fix hasOne relationship method name (#657)

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -155,6 +155,7 @@ class ModelGenerator extends BaseGenerator
             case 'datetime':
                 return 'string|\Carbon\Carbon';
             case '1t1':
+                return '\\'.$this->commandData->config->nsModel.'\\'.$relation->inputs[0].' '.camel_case($relation->inputs[0]);
             case 'mt1':
                 if (isset($relation->inputs[1])) {
                     $relationName = str_replace('_id', '', strtolower($relation->inputs[1]));
