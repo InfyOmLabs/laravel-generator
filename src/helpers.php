9f6b2dafdf5d5c44e023b684a47736c0847ca83f
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-08-03 14:38:34 +0530

    refactor(helpers): use database_path & resource_path helpers (#698)

--- src/helpers.php
+++ src/helpers.php
@@ -88,7 +88,7 @@ if (!function_exists('get_template_file_path')) {
 
         $templatesPath = config(
             'infyom.laravel_generator.path.templates_dir',
-            base_path('resources/infyom/infyom-generator-templates/')
+            resource_path('infyom/infyom-generator-templates/')
         );
 
         $path = $templatesPath.$templateName.'.stub';
