d581b0ab5656cad84a601de0d140c68c0a189b1a
vishalinfyom <vishal.ribdiya@infyom.com>
2019-10-04 11:55:31 +0530

    Revert "refactor: #57 move unique rule to last"
    This reverts commit 981c7d770dec23a9f77de7b2585934c7c28f486f.

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -264,14 +264,6 @@ class ModelGenerator extends BaseGenerator
             }
 
             if (!empty($field->validations)) {
-                if (Str::contains($field->validations, 'unique:')) {
-                    $rule = explode('|', $field->validations);
-                    // move unique rule to last
-                    usort($rule, function ($record) {
-                        return (Str::contains($record, 'unique:')) ? 1 : 0;
-                    });
-                    $field->validations = implode('|', $rule);
-                }
                 $rule = "'".$field->name."' => '".$field->validations."'";
                 $rules[] = $rule;
             }
