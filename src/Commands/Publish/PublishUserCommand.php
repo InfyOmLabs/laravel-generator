84ed27739772ce70745a0254751ee695be7282a8
Shailesh Ladumor <shailesh.infyom@gmail.com>
2019-10-21 16:57:14 +0530

    feat: no repository pattern support added #666

--- src/Commands/Publish/PublishUserCommand.php
+++ src/Commands/Publish/PublishUserCommand.php
@@ -100,6 +100,9 @@ class PublishUserCommand extends PublishBaseCommand
     private function publishUserController()
     {
         $templateData = get_template('user/user_controller', 'laravel-generator');
+        if(!config('infyom.laravel_generator.option.repository_pattern')){
+            $templateData = get_template('user/user_controller_without_repository', 'laravel-generator');
+        }
 
         $templateData = $this->fillTemplate($templateData);
 
