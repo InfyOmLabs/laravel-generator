cfe3359b31b782528c1e4d41483046baf16d68b6
Ajay Makwana <30411745+ajayinfyom@users.noreply.github.com>
2019-11-27 18:21:13 +0530

    fix(relationships): minor fix to generate mtm relationship (#784)

--- src/Utils/TableFieldsGenerator.php
+++ src/Utils/TableFieldsGenerator.php
@@ -456,6 +456,10 @@ class TableFieldsGenerator
             }
         }
 
+        if (empty($manyToManyTable)) {
+            return false;
+        }
+
         $modelName = model_name_from_table_name($manyToManyTable);
 
         return GeneratorFieldRelation::parseRelation('mtm,'.$modelName.','.$tableName);
