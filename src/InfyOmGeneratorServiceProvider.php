a847f8d8f5a6fdf436eb751e202226ad6c2581a2
Mitul Golakiya <mtl.golakiya@gmail.com>
2020-03-06 11:56:42 +0530

    fix(vuejs): old vuejs integration removed

--- src/InfyOmGeneratorServiceProvider.php
+++ src/InfyOmGeneratorServiceProvider.php
@@ -15,13 +15,11 @@ use InfyOm\Generator\Commands\Publish\GeneratorPublishCommand;
 use InfyOm\Generator\Commands\Publish\LayoutPublishCommand;
 use InfyOm\Generator\Commands\Publish\PublishTemplateCommand;
 use InfyOm\Generator\Commands\Publish\PublishUserCommand;
-use InfyOm\Generator\Commands\Publish\VueJsLayoutPublishCommand;
 use InfyOm\Generator\Commands\RollbackGeneratorCommand;
 use InfyOm\Generator\Commands\Scaffold\ControllerGeneratorCommand;
 use InfyOm\Generator\Commands\Scaffold\RequestsGeneratorCommand;
 use InfyOm\Generator\Commands\Scaffold\ScaffoldGeneratorCommand;
 use InfyOm\Generator\Commands\Scaffold\ViewsGeneratorCommand;
-use InfyOm\Generator\Commands\VueJs\VueJsGeneratorCommand;
 
 class InfyOmGeneratorServiceProvider extends ServiceProvider
 {
@@ -110,13 +108,6 @@ class InfyOmGeneratorServiceProvider extends ServiceProvider
             return new RollbackGeneratorCommand();
         });
 
-        $this->app->singleton('infyom.vuejs', function ($app) {
-            return new VueJsGeneratorCommand();
-        });
-        $this->app->singleton('infyom.publish.vuejs', function ($app) {
-            return new VueJsLayoutPublishCommand();
-        });
-
         $this->app->singleton('infyom.publish.user', function ($app) {
             return new PublishUserCommand();
         });
@@ -138,8 +129,6 @@ class InfyOmGeneratorServiceProvider extends ServiceProvider
             'infyom.scaffold.requests',
             'infyom.scaffold.views',
             'infyom.rollback',
-            'infyom.vuejs',
-            'infyom.publish.vuejs',
             'infyom.publish.user',
         ]);
     }
