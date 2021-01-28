6df7b95350d745159789109847842a0c8aab90ba
Saeed Rostami <privatemx.2010@gmail.com>
2021-01-28 06:52:18 +0330

    feat: farsi translation files added (#928)
    * Create auth.php
    
    * Create crud.php
    
    * Create messages.php

---
+++ locale/fa/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => ':model با موفقیت بازیابی شد',
+    'saved'     => ':model با موفقیت ذخیره شد.',
+    'updated'   => ':model با موفقیت به روز شد.',
+    'deleted'   => ':model با موفقیت حذف شد',
+    'not_found' => ':model پیدا نشد',
+
+];
