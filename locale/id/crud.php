4ba5acfe7c922a363d5d05d25a67b1744c06b76f
shailesh_infyom <shailesh.infyom@gmail.com>
2022-01-26 15:09:26 +0530

    feat: Indonesian locale add

---
+++ locale/id/crud.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'add_new'      => 'Tambah baru',
+    'cancel'       => 'Membatalkan',
+    'save'         => 'Menyimpan',
+    'edit'         => 'Sunting',
+    'detail'       => 'Detail',
+    'back'         => 'Kembali',
+    'action'       => 'Tindakan',
+    'id'           => 'Indo',
+    'created_at'   => 'Dibuat di',
+    'updated_at'   => 'Diperbarui Pada',
+    'deleted_at'   => 'Dihapus At',
+    'are_you_sure' => 'Apa kamu yakin?',
+];
