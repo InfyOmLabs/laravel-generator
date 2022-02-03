c00a3eaf54a9b59e581772220145bb2199ad86d5
Shailesh <shailesh.infyom@gmail.com>
2022-02-03 16:23:55 +0530

    feat: Indonesian locale support added (#990)
    * #945 fix spacing and formatting isue
    
    * #945 change let to const
    
    * feat: Indonesian locale add

---
+++ locale/id/auth.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Kredensial ini tidak cocok dengan catatan kami.',
+    'throttle' => 'Terlalu banyak upaya login. Silakan coba lagi dalam :detik detik.',
+
+    'full_name'        => 'Nama lengkap',
+    'email'            => 'Surel',
+    'password'         => 'Kata sandi',
+    'confirm_password' => 'setujui password',
+    'remember_me'      => 'Ingat saya',
+    'sign_in'          => 'Masuk',
+    'sign_out'         => 'Keluar',
+    'register'         => 'Daftar',
+
+    'login' => [
+        'title'               => 'Masuk untuk memulai sesi Anda',
+        'forgot_password'     => 'Saya lupa kata sandi saya',
+        'register_membership' => 'Daftarkan keanggotaan baru',
+    ],
+
+    'registration' => [
+        'title'           => 'Daftarkan keanggotaan baru',
+        'i_agree'         => 'saya setuju untuk',
+        'terms'           => 'syarat-syaratnya',
+        'have_membership' => 'Saya sudah memiliki keanggotaan',
+    ],
+
+    'forgot_password' => [
+        'title'          => 'Masukkan Email untuk mengatur ulang kata sandi',
+        'send_pwd_reset' => 'Kirim Tautan Atur Ulang Kata Sandi',
+    ],
+
+    'reset_password' => [
+        'title'         => 'Mereset password Anda',
+        'reset_pwd_btn' => 'Setel Ulang Kata Sandi',
+    ],
+
+    'confirm_passwords' => [
+        'title'                => 'Harap konfirmasi kata sandi Anda sebelum melanjutkan.',
+        'forgot_your_password' => 'Lupa kata sandi Anda?',
+    ],
+
+    'verify_email' => [
+        'title'       => 'Verifikasi alamat email Anda',
+        'success'     => 'Tautan verifikasi baru telah dikirim ke alamat email Anda',
+        'notice'      => 'Sebelum melanjutkan, periksa email Anda untuk tautan verifikasi. Jika Anda tidak menerima email,',
+        'another_req' => 'klik di sini untuk meminta yang lain',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Klik di sini untuk mengatur ulang kata sandi Anda',
+        ],
+    ],
+
+    'app' => [
+        'member_since' => 'Anggota Sejak',
+        'messages'     => 'Pesan',
+        'settings'     => 'Pengaturan',
+        'lock_account' => 'Kunci Akun',
+        'profile'      => 'Profil',
+        'online'       => 'On line',
+        'search'       => 'Mencari',
+        'create'       => 'Membuat',
+        'export'       => 'Ekspor',
+        'print'        => 'Mencetak',
+        'reset'        => 'Mengatur ulang',
+        'reload'       => 'Muat ulang',
+    ],
+];
---
+++ locale/id/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'retrieved' => ':model berhasil diambil.',
+    'saved'     => ':model berhasil disimpan.',
+    'updated'   => ':model berhasil diperbarui.',
+    'deleted'   => ':model berhasil dihapus.',
+    'not_found' => ':model tidak ditemukan',
+];
