ee2c172d01b4c25198f31685d82de361227becbe
skys215 <skys215@gmail.com>
2022-08-26 22:38:15 +0800

    Added locale/zh_CN

---
+++ locale/zh_CN/auth.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => '用户名或密码错误。',
+    'throttle' => '您尝试的登录次数过多，请 :seconds 秒后再试。',
+    'password' => '密码错误',
+
+    'full_name'        => '姓名',
+    'email'            => '邮箱',
+    'confirm_password' => '确认密码',
+    'remember_me'      => '记住我',
+    'sign_in'          => '登录',
+    'sign_out'         => '退出',
+    'register'         => '注册',
+
+    'login' => [
+        'title'               => '登录以开始会话',
+        'forgot_password'     => '忘记密码？',
+    ],
+
+    'registration' => [
+        'title'           => '创建新账号',
+        'i_agree'         => '我同意',
+        'terms'           => '条款',
+        'have_membership' => '已有账号',
+    ],
+
+    'forgot_password' => [
+        'title'          => '您忘记密码了吗？在这里可以重置您的密码。',
+        'send_pwd_reset' => '发送重置密码邮件',
+    ],
+
+    'reset_password' => [
+        'title'         => '您还差一步即可设置新密码，立即重置密码。',
+        'reset_pwd_btn' => '重置密码',
+    ],
+
+    'confirm_passwords' => [
+        'title'                => '在继续之前请确认您的密码。',
+        'forgot_your_password' => '忘记密码？',
+    ],
+
+    'verify_email' => [
+        'title'       => '验证您的邮箱',
+        'success'     => '已重新发送验证邮件到您的邮箱',
+        'notice'      => '在继续之前，请在邮箱查收验证邮件。如果您未收到，',
+        'another_req' => '点击这里重新发送',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => '点击这里重置密码',
+        ],
+    ],
+];
