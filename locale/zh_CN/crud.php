453d1d364a13b44f7b424dd4cea2c296402db429
skys215 <skys215@gmail.com>
2022-08-27 14:27:03 +0800

    feat: Added locale/zh_CN (#1038)

---
+++ locale/zh_CN/crud.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'add_new'      => '添加',
+    'cancel'       => '取消',
+    'create'       => '创建',
+    'edit'         => '编辑',
+    'save'         => '保存',
+    'detail'       => '详情',
+    'back'         => '返回',
+    'search'       => '搜索',
+    'export'       => '导出',
+    'print'        => '打印',
+    'reset'        => '重置',
+    'reload'       => '刷新',
+    'action'       => '操作',
+    'id'           => 'ID',
+    'created_at'   => '创建时间',
+    'updated_at'   => '更新时间',
+    'deleted_at'   => '删除时间',
+    'are_you_sure' => '您确定吗？',
+];
---
+++ locale/zh_CN/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => '成功获取:model。',
+    'saved'     => '已成功创建:model。',
+    'updated'   => '已成功修改:model。',
+    'deleted'   => '已成功删除:model。',
+    'not_found' => '未找到:model。',
+
+];
