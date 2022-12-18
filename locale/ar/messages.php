497c45e541d504d0855fd49a8388537687f2b42c
Mosaab_Emam <mosaab.emam123@gmail.com>
2022-12-18 16:49:35 +0200

    add locale/ar translation files

---
+++ locale/ar/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => 'تم جلب :model بنجاح.',
+    'saved' => 'تم حفظ :model بنجاح.',
+    'updated' => 'تم تعديل :model بنجاح.',
+    'deleted' => 'تم حذف :model بنجاح.',
+    'not_found' => 'لم يتم العثور على :model.',
+
+];
