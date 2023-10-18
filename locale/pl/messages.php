2e3447b87e68cfb441dd4a23352c7c29c6fdf4ab
Daniel Kamiński <daniel.kaminski123@gmail.com>
2023-10-18 11:36:03 +0200

    Create messages.php

---
+++ locale/pl/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => ':model został pomyślnie odzyskany.',
+    'saved'     => ':model został pomyślnie zapisany.',
+    'updated'   => ':model został pomyślnie zaktualizowany.',
+    'deleted'   => ':model został pomyślnie usunięty.',
+    'not_found' => ':model nie został znaleziony.',
+
+];
