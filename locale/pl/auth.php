a05bf0f42850f47ef11e315a47f6f56f92099d59
Daniel Kamiński <daniel.kaminski123@gmail.com>
2024-01-04 05:03:13 +0100

    feat: Translated into Polish (#1087)
    * Create auth.php
    
    * Create crud.php
    
    * Create messages.php

---
+++ locale/pl/auth.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Poniższe linie języka są używane podczas uwierzytelniania w różnych
+    | komunikatach, które musimy wyświetlić użytkownikowi. Możesz je dowolnie
+    | modyfikować zgodnie z wymaganiami swojej aplikacji.
+    |
+    */
+
+    'failed'   => 'Te dane uwierzytelniające nie pasują do naszych rekordów.',
+    'throttle' => 'Zbyt wiele prób logowania. Proszę spróbuj ponownie za :seconds sekund.',
+
+    'full_name'        => 'Imię i Nazwisko',
+    'email'            => 'Email',
+    'password'         => 'Hasło',
+    'confirm_password' => 'Potwierdź Hasło',
+    'remember_me'      => 'Zapamiętaj mnie',
+    'sign_in'          => 'Zaloguj się',
+    'sign_out'         => 'Wyloguj',
+    'register'         => 'Zarejestruj się',
+
+    'login' => [
+        'title'               => 'Zaloguj się, aby rozpocząć sesję',
+        'forgot_password'     => 'Zapomniałem hasła',
+    ],
+
+    'registration' => [
+        'title'           => 'Zarejestruj nowe konto',
+        'i_agree'         => 'Akceptuję',
+        'terms'           => 'regulamin',
+        'have_membership' => 'Posiadam już konto',
+    ],
+
+    'forgot_password' => [
+        'title'          => 'Zapomniałeś hasła? Tutaj możesz łatwo odzyskać nowe hasło.',
+        'send_pwd_reset' => 'Wyślij link resetujący hasło',
+    ],
+
+    'reset_password' => [
+        'title'         => 'Został Ci tylko jeden krok do nowego hasła, odzyskaj je teraz.',
+        'reset_pwd_btn' => 'Resetuj Hasło',
+    ],
+
+    'confirm_passwords' => [
+        'title'                => 'Proszę potwierdź hasło przed kontynuacją.',
+        'forgot_your_password' => 'Zapomniałeś hasła?',
+    ],
+
+    'verify_email' => [
+        'title'       => 'Zweryfikuj swój adres email',
+        'success'     => 'Na Twój adres email został wysłany świeży link weryfikacyjny.',
+        'notice'      => 'Przed kontynuacją, sprawdź swój email w poszukiwaniu linka weryfikacyjnego. Jeśli nie otrzymałeś emaila,',
+        'another_req' => 'kliknij tutaj, aby poprosić o kolejny',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Kliknij tutaj, aby zresetować swoje hasło',
+        ],
+    ],
+];
---
+++ locale/pl/crud.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'add_new'      => 'Dodaj Nowy',
+    'cancel'       => 'Anuluj',
+    'create'       => 'Utwórz',
+    'edit'         => 'Edytuj',
+    'save'         => 'Zapisz',
+    'delete'       => 'Usuń',
+    'detail'       => 'Szczegóły',
+    'back'         => 'Powrót',
+    'search'       => 'Szukaj',
+    'export'       => 'Eksport',
+    'print'        => 'Drukuj',
+    'reset'        => 'Resetuj',
+    'reload'       => 'Przeładuj',
+    'action'       => 'Akcja',
+    'id'           => 'Id',
+    'created_at'   => 'Utworzono',
+    'updated_at'   => 'Zaktualizowano',
+    'deleted_at'   => 'Usunięto',
+    'are_you_sure' => 'Czy jesteś pewien?',
+];
